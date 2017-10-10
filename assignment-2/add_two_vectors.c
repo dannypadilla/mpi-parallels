11a00f606f76a821f03cea129b75272f4246252c
Danny Padilla <dpadilla010@gmail.com>
2017-10-10 13:15:47 -0700

    finish problem 3.0: add two vectors

---
+++ assignment-2/add_two_vectors.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <mpi.h>
+
+void read_order(int* order,
+		int my_rank, MPI_Comm comm);
+void read_two_vectors(double local_vec_x[], double local_vec_y[], int local_order,
+		 int my_rank, int comm_sz, MPI_Comm comm);
+void print_vector(double local_vec[], int local_order, int order, char title[],
+		  int my_rank, MPI_Comm comm);
+void add_two_vectors(double local_vec_x[], double local_vec_y[], double local_result[], int local_order);
+
+int main(void) {
+  int order, local_order;
+  double *local_vec_x, *local_vec_y, *local_vec_sum;
+
+  int comm_sz, my_rank;
+  MPI_Comm comm;
+
+  MPI_Init(NULL, NULL);
+
+  comm = MPI_COMM_WORLD;
+  MPI_Comm_size(comm, &comm_sz);
+  MPI_Comm_rank(comm, &my_rank);
+
+  // read order
+  read_order(&order, my_rank, comm);
+
+  // calculate how many items each process gets
+  local_order = order / comm_sz;
+
+  // init vector sizes
+  local_vec_x = malloc(local_order * sizeof(double) );
+  local_vec_y = malloc(local_order * sizeof(double) );
+  local_vec_sum = malloc(local_order * sizeof(double) );
+  
+  // read vector x and y
+  read_two_vectors(local_vec_x, local_vec_y, local_order, my_rank, comm_sz, comm);
+  
+  // print x and y
+  print_vector(local_vec_x, local_order, order, "Vector X = ", my_rank, comm);
+  print_vector(local_vec_y, local_order, order, "Vector Y = ", my_rank, comm);
+  
+  // add x + y
+  add_two_vectors(local_vec_x, local_vec_y, local_vec_sum, local_order);
+
+  // print x + y
+  print_vector(local_vec_sum, local_order, order, "X + Y = ", my_rank, comm);
+
+  free(local_vec_x);
+  free(local_vec_y);
+  free(local_vec_sum);
+
+  MPI_Finalize();
+
+  return 0;
+}
+
+void read_order(int* order, int my_rank, MPI_Comm comm) {
+  if(my_rank == 0) {
+    printf("Enter the order(length) of the vectors:\n");
+    scanf("%d", order);
+  }
+  MPI_Bcast(order, 1, MPI_INT, 0, comm);
+}
+
+void read_two_vectors(double local_vec_x[], double local_vec_y[], int local_order,
+		      int my_rank, int comm_sz, MPI_Comm comm) {
+  double* user_input = NULL;
+  int i;
+  
+  if(my_rank == 0) {
+    user_input = malloc(local_order * comm_sz * sizeof(double) );
+    
+    printf("Enter the first Vector:\n");
+    for(i = 0; i < local_order * comm_sz; i++) {
+      scanf("%lf", &user_input[i] );
+    }
+    MPI_Scatter(user_input, local_order, MPI_DOUBLE, local_vec_x, local_order, MPI_DOUBLE, 0, comm);
+
+    printf("Enter the second Vector:\n");
+    for(i = 0; i < local_order * comm_sz; i++) {
+      scanf("%lf", &user_input[i] );
+    }
+    MPI_Scatter(user_input, local_order, MPI_DOUBLE, local_vec_y, local_order, MPI_DOUBLE, 0, comm);
+    
+    free(user_input);
+  } else {
+    MPI_Scatter(user_input, local_order, MPI_DOUBLE, local_vec_x, local_order, MPI_DOUBLE, 0, comm);
+    MPI_Scatter(user_input, local_order, MPI_DOUBLE, local_vec_y, local_order, MPI_DOUBLE, 0, comm);
+  }
+}
+
+void print_vector(double local_vec[], int local_order, int order, char title[],
+		  int my_rank, MPI_Comm comm) {
+  double * vector = NULL;
+  int i;
+
+  if(my_rank == 0) {
+    vector = malloc(order * sizeof(double) );
+    MPI_Gather(local_vec, local_order, MPI_DOUBLE, vector, local_order, MPI_DOUBLE, 0, comm);
+    printf("\n%s\n", title);
+    printf("[ ");
+    for(i = 0; i < order; i++) {
+      printf("%.2lf ", vector[i] );
+    }
+    printf("]\n");
+    free(vector);
+  } else {
+    MPI_Gather(local_vec, local_order, MPI_DOUBLE, vector, local_order, MPI_DOUBLE, 0, comm);
+  }
+}
+
+void add_two_vectors(double local_vec_x[], double local_vec_y[], double local_result[], int local_order) {
+  int local_i;
+  
+  for(local_i = 0; local_i < local_order; local_i++) {
+    local_result[local_i] = local_vec_x[local_i] + local_vec_y[local_i];
+  }
+  
+}
