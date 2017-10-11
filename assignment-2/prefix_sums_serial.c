5c4126863a1c424f90c8324bc2463960c3125cc5
Danny Padilla <dpadilla010@gmail.com>
2017-10-10 22:38:00 -0700

    finish serial version of prefix_sums

--- assignment-2/prefix_sums_serial.c
+++ assignment-2/prefix_sums_serial.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <mpi.h>
 
 void read_n(int* n);
-void read_values(double user_input_values[], int n );
+void read_values(double user_input_values[], int n);
+void calc_partial_sums(double user_input_values[], int n);
+void print_array(double partial_sums[], int n);
 
 int main(void) {
   int n;
@@ -15,7 +16,8 @@ int main(void) {
   user_input_values = malloc(n * sizeof(double) );
   
   read_values(user_input_values, n); // prompt user input values
-  
+  calc_partial_sums(user_input_values, n);
+  print_array(user_input_values, n);
 
   free(user_input_values);
   
@@ -34,5 +36,22 @@ void read_values(double user_input_values[], int n) {
   for(i = 0; i < n; i++) {
     scanf("%lf", &user_input_values[i] );
   }
+}
 
+void calc_partial_sums(double user_input_values[], int n) {
+  int i;
+
+  for(i = 1; i < n; i++) {
+    user_input_values[i] += user_input_values[i - 1];
+  }
+}
+
+void print_array(double partial_sums[], int n) {
+  int i;
+
+  printf("\n[ ");
+  for(i = 0; i < n; i++) {
+    printf("%.2lf ", partial_sums[i] );
+  }
+  printf(" ]\n");
 }
