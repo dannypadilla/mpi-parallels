320a8542c87138fce0d04938f0657bafbf5d0c9c
Danny Padilla <dpadilla010@gmail.com>
2017-11-08 13:30:47 -0800

    add pthread loop

--- assignment-4/4.1_pth_pi_mutex.c
+++ assignment-4/4.1_pth_pi_mutex.c
@@ -4,38 +4,59 @@
 #include <pthread.h>
 #include "timer.h"
 
-/* 4.1
-   Modify the mutex version of the pi calculation program so that the critical section is in the for loop.
-   Compare the performance of your version with the serial version.
-*/
-
 const int MAX_THREADS = 1024;
+
+void* Thread_sum(void* rank);
+/* Only executed by main thread */
+void Get_args(int argc, char* argv[]);
+void Usage(char* prog_name);
+double Serial_pi(long long n);
 
 long thread_count;
 long long n;
 double sum = 0;
 pthread_mutex_t mutex;
 
-void* Thread_sum(void* rank);
-
-/* Only executed by main thread */
-void Get_args(int argc, char* argv[]);
-void Usage(char* prog_name);
-double Serial_pi(long long n);
-
 int main(int argc, char* argv[]) {
   long       thread;  /* Use long in case of a 64-bit system */
   pthread_t* thread_handles;
-  double start, finish, elapsed;
+  double start, finish, elapsed; // for time
 
   /* Get number of threads from command line */
   Get_args(argc, argv);
+
+  thread_handles = malloc(thread_count * sizeof(pthread_t) );
+
+  for(thread = 0; thread < thread_count; thread++) {
+    pthread_create(&thread_handles[thread], NULL, Thread_sum, (void) thread);
+  }
+
+  for(thread = 0; thread < thread_count; thread++) {
+    pthread_join(&thread_handles[thread], NULL);
+  }
+
+  free(thread_handles);
 
   return 0;
 }  /* main */
 
 /*------------------------------------------------------------------*/
 void* Thread_sum(void* rank) {
+
+  long my_rank = (long) rank;
+
+  double my_sum = 0.0;
+  long long i;
+  double factor = 1.0;
+  long long my_first_i = my_n * my_rank;
+  long long my_last_i = my_first_i + my_n;
+
+  for (i = my_first_i; i < my_last_i; i++, factor = -factor) {
+
+    pthread_mutex_lock(&mutex);
+    sum += factor / (2 * i + 1);
+    pthread_mutex_unlock(&mutex);
+  }
    
   return NULL;
 }  /* Thread_sum */
@@ -52,9 +73,9 @@ double Serial_pi(long long n) {
   double factor = 1.0;
 
   for (i = 0; i < n; i++, factor = -factor) {
-    sum += factor/(2*i+1);
+    sum += factor / (2 * i + 1);
   }
-  return 4.0*sum;
+  return 4.0 * sum;
 
 }  /* Serial_pi */
 
@@ -65,11 +86,17 @@ double Serial_pi(long long n) {
  * Globals out: thread_count, n
  */
 void Get_args(int argc, char* argv[]) {
-  if (argc != 3) Usage(argv[0]);
+  if (argc != 3) {
+    Usage(argv[0]);
+  }
   thread_count = strtol(argv[1], NULL, 10);  
-  if (thread_count <= 0 || thread_count > MAX_THREADS) Usage(argv[0]);
+  if (thread_count <= 0 || thread_count > MAX_THREADS) {
+    Usage(argv[0]);
+  }
   n = strtoll(argv[2], NULL, 10);
-  if (n <= 0) Usage(argv[0]);
+  if (n <= 0) {
+    Usage(argv[0]);
+  }
 }  /* Get_args */
 
 
